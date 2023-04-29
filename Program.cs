c4e7f6478f15b979943b314ef560304341218424
Byron Caal <caal97@gmail.com>
2023-04-29 10:14:09 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿// DECLARAMOS LAS VARIABLES
+
+string nombreUsuarioGuardado;
+string contraseñaUsuarioGuardado;
+string nombreUsuario;
+string contraseñaUsuario;
+int intentos = 0;
+bool sesionOK = false;
+
+//Pedimos al usuario ingresar datos  - Crear cuenta usuario
+Console.WriteLine("*******Nueva cuenta de Usuario******");
+Console.Write("Ingrese su usuario");
+nombreUsuarioGuardado = Console.ReadLine();
+Console.Write("Ingrese su contraseña");
+contraseñaUsuarioGuardado = Console.ReadLine();
+Console.WriteLine("******¡Cuentra creada correctamente!");
+
+//Ahora vamos a simular el inicio de sesión
+
+do
+{
+    Console.WriteLine("******Bienvenido al portal web******");
+    Console.WriteLine("Ingrese su usuario");
+    nombreUsuario = Console.ReadLine();
+    Console.Write("Escriba Contraseña: ");
+    contraseñaUsuario = Console.ReadLine();
+
+    if (nombreUsuario.Equals(nombreUsuarioGuardado) && contraseñaUsuario.Equals(contraseñaUsuarioGuardado))
+    { 
+    sesionOK = true;
+        Console.WriteLine("¡Sesion iniciada correctamente");
+    }else
+    {
+        intentos++;
+        Console.WriteLine("Usuario y contraseña incorrecto, vuelva intentarlo");
+    }         
+
+}while(!sesionOK && intentos < 3);
+if (sesionOK)
+{
+    Console.WriteLine("¡Bienvenido al sistema!");
+}else
+{
+    if(intentos >=3)
+    {
+        Console.WriteLine("Haz realizado mas de tres intentos");
+    }
+
+}
+
+
+
+
+
